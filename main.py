f371ceee66f3db10b96e77d4ea8bcb4cdaea0fba
MrTumi <kwonghoiwa@gmail.com>
2024-11-23 16:45:32 +0800

    Add data

--- main.py
+++ main.py
@@ -1,2 +1,150 @@
+import numpy as np
+import pandas as pd
 import os
 import torch
+
+'''Data Pre-processing'''
+data_path = '../data/'
+
+train = pd.read_csv(data_path + 'train.csv')
+test = pd.read_csv(data_path + 'test.csv')
+
+songs = pd.read_csv(data_path + 'songs.csv')
+
+members = pd.read_csv(data_path + 'members.csv', parse_dates=['registration_init_time','expiration_date'])
+
+train = train.merge(songs, on='song_id', how='left')
+
+songs_extra = pd.read_csv(data_path + 'song_extra_info.csv')
+
+test = test.merge(songs, on='song_id', how='left')
+y_all = train['target'].values
+
+members['membership_days'] = members['expiration_date'].subtract(members['registration_init_time']).dt.days.astype(int)
+
+members['registration_year'] = members['registration_init_time'].dt.year
+members['registration_month'] = members['registration_init_time'].dt.month
+members['registration_date'] = members['registration_init_time'].dt.day
+
+members['expiration_year'] = members['expiration_date'].dt.year
+members['expiration_month'] = members['expiration_date'].dt.month
+members['expiration_date'] = members['expiration_date'].dt.day
+members = members.drop(['registration_init_time'], axis=1)
+
+train = train.merge(members, on='msno', how='left')
+test = test.merge(members, on='msno', how = 'left')
+
+def isrc_to_year(isrc):
+    if type(isrc) == str:
+        if int(isrc[5:7]) > 17:
+            return 1900 + int(isrc[5:7])
+        else:
+            return 2000 + int(isrc[5:7])
+    else:
+        return np.nan
+        
+songs_extra['song_year'] = songs_extra['isrc'].apply(isrc_to_year)
+songs_extra.drop(['isrc', 'name'], axis = 1, inplace = True)
+
+train = train.merge(songs_extra, on = 'song_id', how = 'left')
+test = test.merge(songs_extra, on = 'song_id', how = 'left')
+
+def genre_id_count(x):
+    if x == 'no_genre_id':
+        return 0
+    else:
+        return x.count('|') + 1
+        
+train['genre_ids'].fillna('no_genre_id',inplace=True)
+test['genre_ids'].fillna('no_genre_id',inplace=True)
+train['genre_ids_count'] = train['genre_ids'].apply(genre_id_count).astype(np.int8)
+test['genre_ids_count'] = test['genre_ids'].apply(genre_id_count).astype(np.int8)
+
+def lyricist_count(x):
+    if x == 'no_lyricist':
+        return 0
+    else:
+        return sum(map(x.count, ['|', '/', '\\', ';'])) + 1
+    return sum(map(x.count, ['|', '/', '\\', ';']))
+
+train['lyricist'].fillna('no_lyricist',inplace=True)
+test['lyricist'].fillna('no_lyricist',inplace=True)
+train['lyricists_count'] = train['lyricist'].apply(lyricist_count).astype(np.int8)
+test['lyricists_count'] = test['lyricist'].apply(lyricist_count).astype(np.int8)
+
+def composer_count(x):
+    if x == 'no_composer':
+        return 0
+    else:
+        return sum(map(x.count, ['|', '/', '\\', ';'])) + 1
+
+train['composer'].fillna('no_composer',inplace=True)
+test['composer'].fillna('no_composer',inplace=True)
+train['composer_count'] = train['composer'].apply(composer_count).astype(np.int8)
+test['composer_count'] = test['composer'].apply(composer_count).astype(np.int8)
+
+def is_featured(x):
+    if 'feat' in str(x) :
+        return 1
+    return 0
+
+train['artist_name'].fillna('no_artist',inplace=True)
+test['artist_name'].fillna('no_artist',inplace=True)
+train['is_featured'] = train['artist_name'].apply(is_featured).astype(np.int8)
+test['is_featured'] = test['artist_name'].apply(is_featured).astype(np.int8)
+
+def artist_count(x):
+    if x == 'no_artist':
+        return 0
+    else:
+        return x.count('and') + x.count(',') + x.count('feat') + x.count('&')
+
+train['artist_count'] = train['artist_name'].apply(artist_count).astype(np.int8)
+test['artist_count'] = test['artist_name'].apply(artist_count).astype(np.int8)
+
+# if artist is same as composer
+train['artist_composer'] = (train['artist_name'] == train['composer']).astype(np.int8)
+test['artist_composer'] = (test['artist_name'] == test['composer']).astype(np.int8)
+
+# if artist, lyricist and composer are all three same
+train['artist_composer_lyricist'] = ((train['artist_name'] == train['composer']) & (train['artist_name'] == train['lyricist']) & (train['composer'] == train['lyricist'])).astype(np.int8)
+test['artist_composer_lyricist'] = ((test['artist_name'] == test['composer']) & (test['artist_name'] == test['lyricist']) & (test['composer'] == test['lyricist'])).astype(np.int8)
+
+_mean_song_length = np.mean(train['song_length'])
+def smaller_song(x):
+    if x < _mean_song_length:
+        return 1
+    return 0
+
+train['smaller_song'] = train['song_length'].apply(smaller_song).astype(np.int8)
+test['smaller_song'] = test['song_length'].apply(smaller_song).astype(np.int8)
+
+# number of times a song has been played before
+_dict_count_song_played_train = {k: v for k, v in train['song_id'].value_counts().iteritems()}
+_dict_count_song_played_test = {k: v for k, v in test['song_id'].value_counts().iteritems()}
+def count_song_played(x):
+    try:
+        return _dict_count_song_played_train[x]
+    except KeyError:
+        try:
+            return _dict_count_song_played_test[x]
+        except KeyError:
+            return 0
+
+train['count_song_played'] = train['song_id'].apply(count_song_played).astype(np.int64)
+test['count_song_played'] = test['song_id'].apply(count_song_played).astype(np.int64)
+
+# number of times the artist has been played
+_dict_count_artist_played_train = {k: v for k, v in train['artist_name'].value_counts().iteritems()}
+_dict_count_artist_played_test = {k: v for k, v in test['artist_name'].value_counts().iteritems()}
+def count_artist_played(x):
+    try:
+        return _dict_count_artist_played_train[x]
+    except KeyError:
+        try:
+            return _dict_count_artist_played_test[x]
+        except KeyError:
+            return 0
+
+train['count_artist_played'] = train['artist_name'].apply(count_artist_played).astype(np.int64)
+test['count_artist_played'] = test['artist_name'].apply(count_artist_played).astype(np.int64)
