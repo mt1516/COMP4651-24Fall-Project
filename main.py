2c3ed023d453b69631169759873b41ba0c919a0d
MrTumi <kwonghoiwa@gmail.com>
2024-11-23 17:00:34 +0800

    Add LSTM architecture (WIP)

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import math
+import os
 import numpy as np
 import pandas as pd
-import os
+
+import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 '''Data Pre-processing'''
 data_path = '../data/'
@@ -148,3 +154,164 @@ def count_artist_played(x):
 
 train['count_artist_played'] = train['artist_name'].apply(count_artist_played).astype(np.int64)
 test['count_artist_played'] = test['artist_name'].apply(count_artist_played).astype(np.int64)
+
+'''LSTM Model'''
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=8, num_layers=2):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+
+        # setup LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+
+        # setup output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+
+    def forward(self, input, hidden=None):
+        # lstm step => then ONLY take the sequence's final timetep to pass into the linear/dense layer
+        # Note: lstm_out contains outputs for every step of the sequence we are looping over (for BPTT)
+        # but we just need the output of the last step of the sequence, aka lstm_out[-1]
+        lstm_out, hidden = self.lstm(input, hidden)
+        logits = self.linear(lstm_out[-1])              # equivalent to return_sequences=False from Keras
+        genre_scores = F.log_softmax(logits, dim=1)
+        return genre_scores, hidden
+
+    def get_accuracy(self, logits, target):
+        """ compute accuracy for training round """
+        corrects = (
+                torch.max(logits, 1)[1].view(target.size()).data == target.data
+        ).sum()
+        accuracy = 100.0 * corrects / self.batch_size
+        return accuracy.item()
+
+def main(): # WIP
+    model = LSTM(
+        input_dim=33, hidden_dim=128, batch_size=batch_size, output_dim=8, num_layers=2
+    )
+    loss_function = nn.NLLLoss()  # expects ouputs from LogSoftmax
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # To keep LSTM stateful between batches, you can set stateful = True, which is not suggested for training
+    stateful = False
+
+    train_on_gpu = torch.cuda.is_available()
+    if train_on_gpu:
+        print("\nTraining on CUDA GPU")
+    else:
+        print("\nTraining on CPU (CUDA GPU not found)")
+    
+    for epoch in range(num_epochs):
+
+        train_running_loss, train_acc = 0.0, 0.0
+
+        # Init hidden state - if you don't want a stateful LSTM (between epochs)
+        hidden_state = None
+        for i in range(num_batches):
+
+            # zero out gradient, so they don't accumulate btw batches
+            model.zero_grad()
+
+            # train_X shape: (total # of training examples, sequence_length, input_dim)
+            # train_Y shape: (total # of training examples, # output classes)
+            #
+            # Slice out local minibatches & labels => Note that we *permute* the local minibatch to
+            # match the PyTorch expected input tensor format of (sequence_length, batch size, input_dim)
+            X_local_minibatch, y_local_minibatch = (
+                train_X[i * batch_size: (i + 1) * batch_size, ],
+                train_Y[i * batch_size: (i + 1) * batch_size, ],
+            )
+
+            # Reshape input & targets to "match" what the loss_function wants
+            X_local_minibatch = X_local_minibatch.permute(1, 0, 2)
+
+            # NLLLoss does not expect a one-hot encoded vector as the target, but class indices
+            y_local_minibatch = torch.max(y_local_minibatch, 1)[1]
+
+            y_pred, hidden_state = model(X_local_minibatch, hidden_state)  # forward pass
+
+            # Stateful = False for training. Do we go Stateful = True during inference/prediction time?
+            if not stateful:
+                hidden_state = None
+            else:
+                h_0, c_0 = hidden_state
+                h_0.detach_(), c_0.detach_()
+                hidden_state = (h_0, c_0)
+
+            loss = loss_function(y_pred, y_local_minibatch)  # compute loss
+            loss.backward()  # backward pass
+            optimizer.step()  # parameter update
+
+            train_running_loss += loss.detach().item()  # unpacks the tensor into a scalar value
+            train_acc += model.get_accuracy(y_pred, y_local_minibatch)
+
+        print(
+            "Epoch:  %d | NLLoss: %.4f | Train Accuracy: %.2f"
+            % (epoch, train_running_loss / num_batches, train_acc / num_batches)
+        )
+
+        if epoch % 10 == 0:
+            print("Validation ...")  # should this be done every N=10 epochs
+            val_running_loss, val_acc = 0.0, 0.0
+
+            # Compute validation loss, accuracy. Use torch.no_grad() & model.eval()
+            with torch.no_grad():
+                model.eval()
+
+                hidden_state = None
+                for i in range(num_dev_batches):
+                    X_local_validation_minibatch, y_local_validation_minibatch = (
+                        dev_X[i * batch_size: (i + 1) * batch_size, ],
+                        dev_Y[i * batch_size: (i + 1) * batch_size, ],
+                    )
+                    X_local_minibatch = X_local_validation_minibatch.permute(1, 0, 2)
+                    y_local_minibatch = torch.max(y_local_validation_minibatch, 1)[1]
+
+                    y_pred, hidden_state = model(X_local_minibatch, hidden_state)
+                    if not stateful:
+                        hidden_state = None
+
+                    val_loss = loss_function(y_pred, y_local_minibatch)
+
+                    val_running_loss += (
+                        val_loss.detach().item()
+                    )  # unpacks the tensor into a scalar value
+                    val_acc += model.get_accuracy(y_pred, y_local_minibatch)
+
+                model.train()  # reset to train mode after iterationg through validation data
+                print(
+                    "Epoch:  %d | NLLoss: %.4f | Train Accuracy: %.2f | Val Loss %.4f  | Val Accuracy: %.2f"
+                    % (
+                        epoch,
+                        train_running_loss / num_batches,
+                        train_acc / num_batches,
+                        val_running_loss / num_dev_batches,
+                        val_acc / num_dev_batches,
+                    )
+                )
+
+            epoch_list.append(epoch)
+            val_accuracy_list.append(val_acc / num_dev_batches)
+            val_loss_list.append(val_running_loss / num_dev_batches)
+
+    # visualization loss
+    plt.plot(epoch_list, val_loss_list)
+    plt.xlabel("# of epochs")
+    plt.ylabel("Loss")
+    plt.title("LSTM: Loss vs # epochs")
+    plt.show()
+
+    # visualization accuracy
+    plt.plot(epoch_list, val_accuracy_list, color="red")
+    plt.xlabel("# of epochs")
+    plt.ylabel("Accuracy")
+    plt.title("LSTM: Accuracy vs # epochs")
+    # plt.savefig('graph.png')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+    # pass
